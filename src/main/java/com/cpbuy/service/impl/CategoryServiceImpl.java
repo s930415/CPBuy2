f6307f21af9f57847a095124da45ec49d3a3b4f3
s930415 <s930415@gmail.com>
2018-01-29 13:55:59 +0800

    struts + spring

--- src/main/java/com/cpbuy/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/cpbuy/service/impl/CategoryServiceImpl.java
@@ -7,15 +7,19 @@ import com.cpbuy.model.Category;
 import com.cpbuy.service.ICategoryService;;
 
 public class CategoryServiceImpl implements ICategoryService {
-	
+
 	ICategoryDao categoryDao;
+
+	public void setCategoryDao(ICategoryDao categoryDao) {
+		this.categoryDao = categoryDao;
+	}
 
 	/**
 	 * 取得分類列表
 	 * 
 	 * @return
 	 */
-	public List getCategoryList(Integer id){
+	public List getCategoryList(Integer id) {
 		return categoryDao.getCategoryList(id);
 	}
 
