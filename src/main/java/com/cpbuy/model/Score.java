ebecefb27fb7a54e168353752b7ff35049266e70
s930415 <s930415@yahoo.com.tw>
2018-04-10 17:37:50 +0800

    ProductAction
    ProductAction

---
+++ src/main/java/com/cpbuy/model/Score.java
@@ -0,0 +1,42 @@
+package com.cpbuy.model;
+
+public class Score {
+
+	private Integer p_id;
+	private String memo;
+	private String create_date;
+	private double score;
+
+	public Integer getP_id() {
+		return p_id;
+	}
+
+	public void setP_id(Integer p_id) {
+		this.p_id = p_id;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public String getCreate_date() {
+		return create_date;
+	}
+
+	public void setCreate_date(String create_date) {
+		this.create_date = create_date;
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+}
